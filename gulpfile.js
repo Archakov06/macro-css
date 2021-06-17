ad0a2268caf7553d41aaad20ac4dfde4c2fe22ad
Archakov Dennis <amonbower@gmail.com>
2021-06-17 03:02:02 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+gulp.task('build', () => {
+  return gulp
+    .src('./src/atomcss.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./src/*.scss', gulp.parallel('build'));
+});
